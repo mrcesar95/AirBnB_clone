5c8a78199f7a84d3f64fa7f3dca77a6b336f6622
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-01 14:59:57 -0500

    Adding quit, EOF, empty line

--- console.py
+++ console.py
@@ -4,8 +4,6 @@ Class CommandConsole for Airbnb
 """
 import cmd
 from datetime import datetime
-
-from click import prompt
 from models.base_model import BaseModel
 from models.place import Place
 from models.state import State
@@ -25,3 +23,16 @@ class HBNBCommand(cmd.Cmd):
 	HBNB Class
 	"""
 	prompt = '(hbnb) '
+
+	def do_quit(self, line):
+		"""quit command: exit the program"""
+
+	def do_EOF(self, line):
+		"""End of File command: exit the program"""
+
+	def emptyline(self):
+		"""overridden to not do nothing"""
+	pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

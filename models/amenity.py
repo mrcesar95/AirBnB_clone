ff6731021dded484fc001c99049da0e81f7393dc
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-03 17:07:12 -0500

    Update all

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+
+    name = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+
+    place_id = ""
+    user_id = ""
+    text = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+                        

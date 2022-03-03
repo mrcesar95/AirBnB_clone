c5f28a872123c6caca1571b88bb74120c1798ac1
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-03 13:40:24 -0500

    update files because Que galletica

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-from models.engine import file_storage
-from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+
+storage = FileStorage()
 storage.reload()

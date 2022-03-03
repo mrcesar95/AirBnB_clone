bc9c2a4a3db01a349536d3232ce07801671995a8
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-03 10:21:10 -0500

    Updating __init__.py of models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-from models.engine.file_storage import FileStorage
+from models.engine import file_storage
+from models.base_model import BaseModel
 
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()

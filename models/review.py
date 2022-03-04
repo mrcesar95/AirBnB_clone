de2e04082c7a2d16eba06bd88c60cbfe02175b7b
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-04 11:34:40 -0500

    Adding test_review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""Class Review"""
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-
-    place_id = ""
-    user_id = ""
-    text = ""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-                        
+    """Represents a Review"""
+    place_id = ''
+    user_id = ''
+    text = ''

44354bb25a7aee728cd8a8b8db386ab49a0c4636
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-05 17:50:02 -0500

    Updating models files

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Class City"""
 from models.base_model import BaseModel
 
-class City(BaseModel):
 
-    state_id = ""
-    name = ""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class City(BaseModel):
+    """Represents a City"""
+    state_id = ''
+    name = ''
--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-""" Creation of class user"""
+"""Class User"""
 from models.base_model import BaseModel
 
-class User(BaseModel):
 
+class User(BaseModel):
+    """Represents a User"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)

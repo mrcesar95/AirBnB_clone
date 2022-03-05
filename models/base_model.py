02cea1a52dc88753939813bae21a5647bcf3b0f1
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-05 11:29:09 -0500

    [Update] - Pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 from datetime import datetime
 import models
 
+
 class BaseModel():
     """ Class BaseModel that defines all common
     attributes y methods"""
@@ -14,18 +15,20 @@ class BaseModel():
             del kwargs['__class__']
             for key, value in kwargs.items():
                 setattr(self, key, value)
-            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(
+                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.strptime(
+                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid4())
             self.updated_at = datetime.now()
             self.created_at = datetime.now()
             models.storage.new(self)
-            models.storage.save() 
+            models.storage.save()
 
         if args is not None:
             pass
-    
+
     def __str__(self):
         """ Representation string of class BaseModel"""
         __name = type(self).__name__
@@ -35,10 +38,11 @@ class BaseModel():
         """ Save the object into .json file"""
         self.updated_at = datetime.now()
         models.storage.save()
-        
+
     def to_dict(self):
         """ return an dictionary representation of object"""
         dictionary = dict(self.__dict__)
         dictionary["__class__"] = self.__class__.__name__
-        dictionary.update({'created_at' : self.created_at.isoformat(), 'updated_at' : self.updated_at.isoformat()})
+        dictionary.update({'created_at': self.created_at.isoformat(),
+                           'updated_at': self.updated_at.isoformat()})
         return dictionary

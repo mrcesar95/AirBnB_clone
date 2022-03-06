58e7e283e6fb47f3c3e4e97dfba80645b160417e
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-06 15:20:37 -0500

    Updating amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,6 @@ class Amenity(BaseModel):
     """Represents a City"""
     name = ''
 
-    def __ini__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """"""
-        super().__ini__(*args, **kwargs)
+        super().__init__(*args, **kwargs)

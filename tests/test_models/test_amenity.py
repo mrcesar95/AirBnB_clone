97a18aac7f7340acc5614220f283022d147eceec
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-06 14:34:20 -0500

    [Update]

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -31,15 +31,7 @@ class TestAmenityDocs(unittest.TestCase):
                          "Amenity class needs a docstring")
         self.assertTrue(len(Amenity.__doc__) >= 1,
                         "Amenity class needs a docstring")
-        
-    def test_city_func(self):
-        """test cases for class object Amenity"""
-        for func in self.city_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
-            
+                    
             
 class TestAmenity(unittest.TestCase):
     """test cases for class object Amenity"""
